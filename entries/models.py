89a84c5a802e049c4e9c01fc8a12de86090eefb8
Ujjawal Srivastava <srivastava.ujjawal011@gmail.com>
2022-04-28 18:23:26 +0530

    Bug Fix

--- entries/models.py
+++ entries/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Entry(models.Model):
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, default=User.objects.first().pk)
+        User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
     date_created = models.DateField()
